8806927bfe8138dca41b5ee9c07c3c6e1f4c69ae
Beartrap <beartrap@lizs-air.lan>
2018-01-30 22:16:44 -0800

    Initial commit

---
+++ vpython_L1.py
@@ -0,0 +1,8 @@
+from visual import *
+
+sphere(pos = vector(5,0,0), radius = 0.5, color = color.cyan)
+sphere(pos = vector(0,5,0), radius = 0.5, color = color.green)
+sphere(pos = vector(-5,0,0), radius = 0.5, color = color.blue)
+arrow(pos = vector (5,0,0), axis=vector(-1,-1,0), color = color.red)
+arrow(pos = vector (0,5,0), axis=vector(+1,+1,0), color = color.yellow)
+arrow(pos = vector (-5,0,0), axis=vector(+1,+1,0), color = color.magenta)
